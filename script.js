d15c6b4d24570156c5e3cc046f1c7109142ca9c8
RhuanSchmidt0505 <104856922+RhuanSchmidt0505@users.noreply.github.com>
2024-04-26 09:53:08 -0300

    .

---
+++ script.js
@@ -0,0 +1,35 @@
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Função para calcular o tempo até o aniversário
+function calcularTempoAteAniversario(dataAniversario) {
+    var hoje = new Date();
+    var aniversarioEsteAno = new Date(hoje.getFullYear(), dataAniversario.getMonth(), dataAniversario.getDate());
+    if (hoje > aniversarioEsteAno) {
+        aniversarioEsteAno.setFullYear(aniversarioEsteAno.getFullYear() + 1);
+    }
+    var diferenca = aniversarioEsteAno - hoje;
+    var dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+    return dias;
+}
+
+// Defina as datas dos aniversários
+var seuAniversario = new Date("2024-08-01");
+var aniversarioColega1 = new Date("2024-10-15");
+var aniversarioColega2 = new Date("2024-12-25");
+
+// Exiba os tempos até os aniversários
+document.getElementById("seuAniversarioCountdown").innerHTML = calcularTempoAteAniversario(seuAniversario) + " dias";
+document.getElementById("colega1Countdown").innerHTML = calcularTempoAteAniversario(aniversarioColega1) + " dias";
+document.getElementById("colega2Countdown").innerHTML = calcularTempoAteAniversario(aniversarioColega2) + " dias";
